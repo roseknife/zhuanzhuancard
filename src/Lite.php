7795d94ba8d20d1ca3440208f98ca3b327e60a32
yuetao <1181200882@qq.com>
2018-11-14 11:44:45 +0800

    添加查询接口

--- src/Lite.php
+++ src/Lite.php
@@ -98,6 +98,47 @@ class Lite
         return $result;
     }
 
+    /**
+     * 转转卡退款
+     * @param $trade object 退款对象OrderRefund
+     * @return mixed
+     */
+    public function orderRefund2($trade){
+        $obj = array_filter((array)$trade); //转数组移除空值
+        $obj['channelCode'] = $this->CHANNEL_CODE; //添加渠道值
+        ksort($obj); //进行排序
+        $signStr = "";
+        foreach ($obj as $k => $v) {
+            $signStr .= strtoupper($k . "=" . $v . "&");
+        }
+        $signStr .= "CHANNELKEY=" . $this->CHANNELKEY;
+        $obj['token'] = strtoupper(md5($signStr)); //设置签名
+        ksort($obj);
+        $data = json_encode($obj);
+
+        $url = 'http://61.155.179.185/interface/orderinqury';
+        $headers = array(
+            "Content-type: application/json;charset='utf-8'",
+            "Accept: application/json",
+            "Cache-Control: no-cache",
+            "Pragma: no-cache"
+        );
+        $ch = curl_init();
+        $timeout = 60;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_PORT,5001);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);  // 从证书中检查SSL加密算法是否存在
+        $handles = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($handles, true);
+    }
+
 
     public function orderinqury($trade){
         $obj = array_filter((array)$trade); //转数组移除空值
